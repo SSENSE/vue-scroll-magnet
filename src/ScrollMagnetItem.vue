73aa76887746c59e5e6c5a0c3928c84d8e9cd60c
Todd Beauchamp <toddbee@gmail.com>
2017-01-12 15:53:34 -0500

    chore(lint): update rules to eslint-config-ssense

--- src/ScrollMagnetItem.vue
+++ src/ScrollMagnetItem.vue
@@ -9,7 +9,7 @@
 
 <script>
   export default {
-    name: 'scroll-magnet-item',
+    name: "scroll-magnet-item",
     data() {
       return {
         nearestContainer: undefined,
@@ -20,7 +20,7 @@
         scrollEnd: 0,
         isBottomed: false,
         isScrolling: false,
-      };
+      }
     },
     props: {
       offsetTopPad: {
@@ -30,26 +30,26 @@
       },
     },
     created() {
-      this.nearestContainer = this.getNearestMagnetContainer();
+      this.nearestContainer = this.getNearestMagnetContainer()
     },
     mounted() {
       // Get and set initial state dependend on DOM attributes
-      this.setMagnetHeight();
+      this.setMagnetHeight()
 
       this.$nextTick(() => {
-        this.setMagnetStatus(this.nearestContainer);
-        this.setMagnetWidth();
-      });
+        this.setMagnetStatus(this.nearestContainer)
+        this.setMagnetWidth()
+      })
 
-      // Listen on changes to the parent container's width and update the item's width accordingly
-      this.$watch('nearestContainer.width', () => {
-        this.width = this.nearestContainer.width;
-      });
+      // Listen on changes to the parent container"s width and update the item"s width accordingly
+      this.$watch("nearestContainer.width", () => {
+        this.width = this.nearestContainer.width
+      })
 
-      // Listen on changes to the scroll position from the parent and update the magnet's status
-      this.$watch('nearestContainer.scrollTop', () => {
-        this.setMagnetStatus(this.nearestContainer);
-      });
+      // Listen on changes to the scroll position from the parent and update the magnet"s status
+      this.$watch("nearestContainer.scrollTop", () => {
+        this.setMagnetStatus(this.nearestContainer)
+      })
     },
     methods: {
       /**
@@ -57,49 +57,63 @@
        * @return {object} The matching Vue component object
        */
       getNearestMagnetContainer() {
-        return this.checkParentForMatch(this.$parent);
+        return this.checkParentForMatch(this.$parent)
       },
       /**
        * Recursively lookup nearest parent which is a scroll magnet container
-       * @param  {object} parent The parent's Vue component object
+       * @param  {object} parent The parent"s Vue component object
        * @return {object}        The matching Vue component object
        */
       checkParentForMatch(parent) {
         return (
-          parent && parent.$options._componentTag === 'scroll-magnet-container' // eslint-disable-line no-underscore-dangle,max-len
-          ) ? parent : this.checkParentForMatch(parent);
+          parent && parent.$options._componentTag === "scroll-magnet-container" // eslint-disable-line no-underscore-dangle,max-len
+          ) ? parent : this.checkParentForMatch(parent)
       },
       /**
-       * Assign the magnet's width to match the parent's width
+       * Assign the magnet"s width to match the parent"s width
        * This will be used as an inline CSS property on the magnet
        */
       setMagnetWidth() {
-        this.width = this.nearestContainer.width;
+        this.width = this.nearestContainer.width
       },
       /**
-       * Assign the magnet's height to match its content
+       * Assign the magnet"s height to match its content
        * This is used as a reference for how tall the magnet is while scrolling
        */
       setMagnetHeight() {
-        const viewportOffset = this.$el.getBoundingClientRect();
+        const viewportOffset = this.$el.getBoundingClientRect()
 
-        this.height = viewportOffset.height;
+        this.height = viewportOffset.height
       },
       /**
-       * Update the magnet's status while scrolling
+       * Update the magnet"s status while scrolling
        */
       setMagnetStatus(nearestContainer) {
-        this.setMagnetHeight(); // Recheck for any changes in magnet height
+        this.setMagnetHeight() // Recheck for any changes in magnet height
 
-        this.scrollDist = (nearestContainer.scrollTop + this.height); // The distance scrolled within the bounds (Number)
-        this.scrollEnd = (nearestContainer.offsetTop + (nearestContainer.height - this.offsetTopPad)); // The maximum scrollable distance (Number)
-        this.isWithinHeight = (this.scrollDist < this.scrollEnd); // Is the item still within the bounds? (Boolean)
-        this.isScrolling = ((nearestContainer.scrollTop + this.offsetTopPad) >= nearestContainer.offsetTop && this.isWithinHeight); // Is the item currently scrolling? (Boolean)
-        this.isBottomed = (this.scrollDist >= this.scrollEnd); // Should the item stick to the bottom of the bounds? (Boolean)
-        this.top = (this.isBottomed ? 'auto' : `${this.offsetTopPad}px`); // Item's CSS top value (Number)
+        // The distance scrolled within the bounds (Number)
+        this.scrollDist = (nearestContainer.scrollTop + this.height)
+
+        // The maximum scrollable distance (Number)
+        this.scrollEnd = (nearestContainer.offsetTop + (nearestContainer.height - this.offsetTopPad))
+
+        // Is the item still within the bounds? (Boolean)
+        this.isWithinHeight = (this.scrollDist < this.scrollEnd)
+
+        // Is the item currently scrolling? (Boolean)
+        this.isScrolling = (
+          (nearestContainer.scrollTop + this.offsetTopPad)
+          >= nearestContainer.offsetTop && this.isWithinHeight
+        )
+
+        // Should the item stick to the bottom of the bounds? (Boolean)
+        this.isBottomed = (this.scrollDist >= this.scrollEnd)
+
+        // Item"s CSS top value (Number)
+        this.top = (this.isBottomed ? "auto" : `${this.offsetTopPad}px`)
       },
     },
-  };
+  }
 </script>
 
 <style scoped>
